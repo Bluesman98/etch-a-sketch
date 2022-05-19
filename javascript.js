b52f967875dbdc093b237af5289ff57a9a5f8654
Bluesman98 <papaderos1998@gmail.com>
2022-05-19 22:08:21 +0300

    add event listener for mouseover and make grid cell change color when event triggers

--- javascript.js
+++ javascript.js
@@ -1,12 +1,15 @@
 const body = document.body;
 const container = document.createElement("div");
 container.classList.add("grid-container");
-container.setAttribute('grid-template-rows', 'repeat(16, 1fr)');
 for(i = 0; i < 256; i++ ) {
     let div = document.createElement("div");
-    div.textContent = `${i+1}`;
+   // div.textContent = `${i+1}`;
     div.classList.add("grid-item");
     container.append(div);
 }
 
 body.append(container);
+const grid =document.querySelectorAll('.grid-item');
+grid.forEach(e => e.addEventListener('mouseover', event => {
+    e.classList.add('grid-active');
+}))
