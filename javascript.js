f4e03087ff21e838cc33a139dda1f3a425bf7edf
Bluesman98 <papaderos1998@gmail.com>
2022-05-20 13:10:59 +0300

    add the ability to change the grid size via button

--- javascript.js
+++ javascript.js
@@ -1,15 +1,36 @@
 const body = document.body;
 const container = document.createElement("div");
 container.classList.add("grid-container");
-for(i = 0; i < 256; i++ ) {
-    let div = document.createElement("div");
-   // div.textContent = `${i+1}`;
-    div.classList.add("grid-item");
-    container.append(div);
-}
+
 
 body.append(container);
-const grid =document.querySelectorAll('.grid-item');
-grid.forEach(e => e.addEventListener('mouseover', event => {
-    e.classList.add('grid-active');
-}))
+
+function createGrid(size, container) {
+    container.style.gridTemplateColumns = `repeat(${size},50px)`;
+    for(i = 0; i < size*size; i++ ) {
+        let div = document.createElement("div");
+        div.classList.add("grid-item");
+        container.append(div);
+    }
+}
+createGrid(16,container);
+
+
+
+function newGrid(container) {
+   const  a = document.querySelector(".grid-container");
+   while (a.firstChild) {
+     a.removeChild(a.lastChild);
+   }
+   let size = prompt("Enter Size Of Grid");
+   createGrid(size,container);
+}
+
+
+container.addEventListener('mouseover', function(e) {
+    if(e.target && e.target.classList.contains("grid-item") == true )  {
+        e.target.classList.add('grid-active');
+    }
+});
+
+
