2f10dfb57f67b449a7489acce02989a88e952bff
Bluesman98 <papaderos1998@gmail.com>
2022-05-20 15:17:01 +0300

    Grid cells will now resize to fill the container space depending on the size of the grid

--- javascript.js
+++ javascript.js
@@ -1,21 +1,17 @@
 const body = document.body;
 const container = document.createElement("div");
 container.classList.add("grid-container");
-
-
 body.append(container);
 
 function createGrid(size, container) {
-    container.style.gridTemplateColumns = `repeat(${size},50px)`;
+    container.style.gridTemplateColumns = `repeat(${size},1fr)`;
+    container.style.gridTemplateRows = `repeat(${size},1fr)`;
     for(i = 0; i < size*size; i++ ) {
         let div = document.createElement("div");
         div.classList.add("grid-item");
         container.append(div);
     }
 }
-createGrid(16,container);
-
-
 
 function newGrid(container) {
    const  a = document.querySelector(".grid-container");
@@ -23,9 +19,9 @@ function newGrid(container) {
      a.removeChild(a.lastChild);
    }
    let size = prompt("Enter Size Of Grid");
-   createGrid(size,container);
+   if(size <= 64) createGrid(size,container);
+   else alert("Please enter size < 64!")
 }
-
 
 container.addEventListener('mouseover', function(e) {
     if(e.target && e.target.classList.contains("grid-item") == true )  {
